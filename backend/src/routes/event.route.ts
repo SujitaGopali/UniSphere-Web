2a45469616135f75e00c0738f3b554420a00024b
SujitaGopali <166592976+SujitaGopali@users.noreply.github.com>
2026-07-28 16:31:37 +0545

    updated event routes

--- backend/src/routes/event.route.ts
+++ backend/src/routes/event.route.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { EventController } from "../controllers/event.controller";
 import { EventMongoRepository } from "../repositories/event.repository";
 import { EventService } from "../services/event.service";
-import { authorizedMiddleware } from "../middlewares/authorized.middleware";
+import { authorizedMiddleware, adminMiddleware } from "../middlewares/authorized.middleware";
 
 const eventRepository = new EventMongoRepository();
 const eventService = new EventService(eventRepository);
@@ -12,8 +12,8 @@ const router = Router();
 
 router.get("/", eventController.getAllEvents);
 router.get("/:id", eventController.getEventById);
-router.post("/", authorizedMiddleware, eventController.createEvent);
-router.put("/:id", authorizedMiddleware, eventController.updateEvent);
-router.delete("/:id", authorizedMiddleware, eventController.deleteEvent);
+router.post("/", authorizedMiddleware, adminMiddleware, eventController.createEvent);
+router.put("/:id", authorizedMiddleware, adminMiddleware, eventController.updateEvent);
+router.delete("/:id", authorizedMiddleware, adminMiddleware, eventController.deleteEvent);
 
 export default router;
